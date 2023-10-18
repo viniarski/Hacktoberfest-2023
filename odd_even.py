f4256fbd7817d3f4857e0fd78050739ccdf7c816
viniarski <41969647+viniarski@users.noreply.github.com>
2023-10-18 14:57:54 +0100

    Create odd_even.py
    prints list of odd and even numbers from 1 - 100

---
+++ odd_even.py
@@ -0,0 +1,7 @@
+# prints list of numbers from 1 to 100
+odd_numbers = list(range(1, 101, 2))
+print(odd_numbers)
+
+# prints list of even numbers from 2 to 100
+even_numbers = list(range(2, 101, 2))
+print(even_numbers)
